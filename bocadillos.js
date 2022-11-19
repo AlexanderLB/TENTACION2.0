c13a9f5182b2c0d5826611e9a75796ee4cb6d319
AlexanderLB <51377574+AlexanderLB@users.noreply.github.com>
2022-11-19 22:52:03 +0100

    Add files via upload

---
+++ bocadillos.js
@@ -0,0 +1,30 @@
+/*
+function Ocultar(bx){
+    vista=document.getElementById(bx).style.display;
+    if (vista=='none')
+    vista='block';
+    else
+    vista='none';
+    document.getElementById(bx).style.display = vista;
+}
+
+function core(bz){
+    vista=document.getElementById(bz).style.display;
+    if (vista=='none')
+    vista='block';
+    else
+    vista='none';
+    document.getElementById(bz).style.display = vista;
+}
+*/
+
+function mostrar(bc){
+    switch (bc) {
+        case 1:
+            $('bc').$('mostrar')
+            break;
+    
+        default:
+            break;
+    }
+}
